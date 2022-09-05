9f41bcf2afa7e30010227d01c12b8637681b33bc
ievhens <ievhen@genovawebart.com>
2022-09-05 17:08:12 +0300

    datepicker jquery customized

--- assets/delille.js
+++ assets/delille.js
@@ -5,6 +5,17 @@ $(document).ready(function(){
         "autoplaySpeed": 5,
         "fadeSpeed": 3000,
         "hoverStop": true});
+
+    let holydays = ["9-20-2022", "9-28-2022"];
+
+    function lockHolidays(date) {
+        let month = date.getMonth(), day = date.getDate(), year = date.getFullYear(), weekday = date.getDay();
+        if($.inArray(`${month + 1}-${day}-${year}`, holydays) != -1 || new Date() > date || weekday == 1 ){return [false]};
+        return [true];
+    }
+    $('[name="delivery-date"]').datepicker({
+        beforeShowDay: lockHolidays
+    })
 });
 
 
